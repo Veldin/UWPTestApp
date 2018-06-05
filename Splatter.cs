6db47fc97e6cd27f4584dcc0dea2914e309b1af1
jaroen007 <jaroeneefting@hotmail.com>
2018-06-05 14:41:04 +0200

    Add files via upload
    Add splatter.cs

---
+++ Splatter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPTestApp
+{
+    public class Splatter : GameObject
+    {
+        public Splatter(float width, float height, float fromLeft, float fromTop, float widthDrawOffset = 0, float heightDrawOffset = 0, float fromLeftDrawOffset = 0, float fromTopDrawOffset = 0)
+        : base(width, height, fromLeft, fromTop, widthDrawOffset, heightDrawOffset, fromLeftDrawOffset, fromTopDrawOffset)
+        {
+
+        }
+
+        //Any object can edit the gameObjects of the game while the logic is running.
+        //And Also get the delta for timed events.
+        public override Boolean OnTick(ArrayList gameObjects, float delta)
+        {
+            return true;
+        }
+
+        public override Boolean CollitionEffect(GameObject gameObject)
+        {
+            return true;
+        }
+    }
+}
