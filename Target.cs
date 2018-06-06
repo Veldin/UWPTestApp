d96a9ab15c164392ed6a84daaf242f16f67a8403
Rickmarges <rickmarges@gmail.com>
2018-06-06 14:01:27 +0200

    My Files

---
+++ Target.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class Target
+{
+    float fromTop;
+    float fromLeft;
+    Targetable target;
+
+    Target(Targetable target)
+    {
+        this.target = target;
+    }
+
+    Target(float fromTop, float fromLeft)
+    {
+        this.fromTop = fromTop;
+        this.fromLeft = fromLeft;
+    }
+
+    public void SetTarget(GameObject target)
+    {
+        this.target = target;
+    }
+
+    public void SetFromLeft(float fromLeft)
+    {
+        this.fromLeft = fromLeft;
+    }
+
+    public void SetFromTop(float fromTop)
+    {
+        this.fromTop = fromTop;
+    }
+
+    public float GetFromTop()
+    {
+        return fromTop;
+    }
+
+    public float GetFromLeft()
+    {
+        return fromLeft;
+    }
+}
