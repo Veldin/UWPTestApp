20adfa7d14311dba96ef479361d23a04b11ff464
Rickmarges <rickmarges@gmail.com>
2018-06-07 14:17:14 +0200

    Added movable object

---
+++ EindopdrachtUWP/Classes/Player.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+public class Player : MovableObject, Targetable
+{
+    private int walkSpeed;
+    private int health;
+    private int armor;
+    private int level;
+
+    public Player()
+    {
+        walkSpeed = 20;
+        health = 100;
+        armor = 0;
+        level = 1;
+    }
+
+    public void IncreaseHealth(int amount)
+    {
+        health += amount;
+    }
+
+    public void IncreaseArmor(int amount)
+    {
+        armor += amount;
+    }
+
+    public void IncreaseLevel()
+    {
+        level++;
+    }
+
+    public int GetLevel() => level;
+
+    public override Boolean CollitionEffect(GameObject gameObject)
+    {
+        if (gameObject.HasTag("iets"))
+        {
+            PlayHitSound();
+        }
+
+        return true;
+    }
+
+    public float MovableObject.GetMovementSpeed()
+    {
+        return walkSpeed;
+    }
+
+    public void MovableObject.SetMoveSound(MediaElement moveSound)
+    {
+        this.moveSound = moveSound;
+    }
+
+    public void MovableObject.PlayMoveSound()
+    {
+        
+    }
+
+    public void MovableObject.SetDeathSound(MediaElement deathSound)
+    {
+        this.deathSound = deathSound;
+    }
+
+    public void MovableObject.PlayDeathSound()
+    {
+
+    }
+}
---
+++ EindopdrachtUWP/Classes/Projectile.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+public class Projectile : MovableObject
+{
+    float shotFromTop;
+    float shotFromLeft;
+    float damage;
+    float movementSpeed;
+    MediaElement moveSound;
+    MediaElement deathSound;
+
+    Projectile()
+    {
+        damage = 20;
+        shotFromTop = 540;
+        shotFromLeft = 960;
+    }
+
+    public override Boolean CollitionEffect(GameObject gameObject)
+    {
+        if (gameObject.HasTag("iets"))
+        {
+            PlayHitSound();
+        }
+
+        return true;
+    }
+
+    public void SetNewTarget()
+    {
+        Target target = new Target();
+    }
+
+    public float GetShotFromTop()
+    {
+        return shotFromTop;
+    }
+
+    public float GetShotFromLeft()
+    {
+        return shotFromLeft;
+    }
+
+    public override Boolean OnTick(List<GameObject> gameObjects, float delta)
+    {
+        return true;
+    }   
+
+    public void MovableObject.SetMovementSpeed(float speed)
+    {
+        movementSpeed = speed;
+    }
+
+    public float MovableObject.GetMovementSpeed()
+    {
+        return movementSpeed;
+    }
+
+    public void MovableObject.SetMoveSound(MediaElement moveSound)
+    {
+        this.moveSound = moveSound;
+    }
+
+    public void MovableObject.PlayMoveSound()
+    {
+        
+    }
+
+    public void MovableObject.SetDeathSound(MediaElement deathSound)
+    {
+        this.deathSound = deathSound;
+    }
+
+    public void MovableObject.PlayDeathSound()
+    {
+        
+    }
+}
