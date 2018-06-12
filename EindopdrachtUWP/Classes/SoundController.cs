0679b20911f6cd7902795e425d00c11428f8b088
alwintje <alwinkroesen@gmail.com>
2018-06-12 23:06:17 +0200

    SoundController

--- EindopdrachtUWP/Classes/SoundController.cs
+++ EindopdrachtUWP/Classes/SoundController.cs
@@ -8,15 +8,46 @@ using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
-namespace UWPTestApp
+namespace EindopdrachtUWP.Classes
 {
 
 
     class SoundController
     {
+        private Dictionary<string, MediaElement> sounds;
+
+        public SoundController()
+        {
+            sounds = new Dictionary<string, MediaElement>();
+        }
+
+        public void AddSound(string sound)
+        {
+            MediaElement me = new MediaElement();
+            me.autoplay = false;
+            sounds.add(sound, me);
+        }
+
+        public void PlaySound(string sound)
+        {
+            if(sounds.ContainsKey(sound))
+            {
+                Play(sounds[sound]);
+            }
+
+        }
+
+        public void LoadAllSounds()
+        {
+            foreach (KeyValuePair<string, MediaElement> sound in sounds)
+            {
+                LoadSound(entry.Key, entry.Value);
+            }
+        }
+
 
         // Must be in another method (because of async)
-        public static async void LoadSound(MediaElement sound, String filename)
+        public async void LoadSound(MediaElement sound, String filename)
         {
             Windows.Storage.StorageFolder folder = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFolderAsync(@"Assets");
             Windows.Storage.StorageFile file = await folder.GetFileAsync(filename);
@@ -24,7 +55,7 @@ namespace UWPTestApp
             sound.SetSource(stream, file.ContentType);
         }
 
-        public static void Play(MediaElement sound)
+        public void Play(MediaElement sound)
         {
             try
             {
