840f910b3d92d59fe45d7d61f427ad0c92520f0c
jaroen007 <jaroeneefting@hotmail.com>
2018-06-15 14:11:34 +0200

    difficulty delta
    difficulty delta

--- EindopdrachtUWP/Classes/Spawner.cs
+++ EindopdrachtUWP/Classes/Spawner.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace UWPTestApp
 {
@@ -8,7 +9,7 @@ namespace UWPTestApp
         protected float beginDelta;                 //The delta till this spawner starts.
         protected float cooldownDelta;              //The max delta it takes to spawn the next, after it spawned a guy.
         protected float remainingCooldownDelta;     //the delta it takes to spawn the next.
-
+        protected float difficultyDelta;            //The delta to increase difficulty as time goes on.
 
         public Spawner(float width, float height, float fromLeft, float fromTop, float widthDrawOffset = 0, float heightDrawOffset = 0, float fromLeftDrawOffset = 0, float fromTopDrawOffset = 0, float beginDelta = 3000, float cooldownDelta = 4000)
         : base(width, height, fromLeft, fromTop, widthDrawOffset, heightDrawOffset, fromLeftDrawOffset, fromTopDrawOffset)
@@ -16,6 +17,7 @@ namespace UWPTestApp
             this.beginDelta = beginDelta;
             this.cooldownDelta = cooldownDelta;
             this.remainingCooldownDelta = 0;
+            this.difficultyDelta = 0;
         }
 
         public float BeginDelta
@@ -40,6 +42,9 @@ namespace UWPTestApp
         //And Also get the delta for timed events.
         public override Boolean OnTick(List<GameObject> gameObjects, float delta)
         {
+            difficultyDelta += delta;
+            Debug.WriteLine("lol: "+ difficultyDelta);
+
 
             if (BeginDelta > 0 )
             {
@@ -61,6 +66,8 @@ namespace UWPTestApp
 
                 //Spawn an anemy
                 gameObjects.Add(new Enemy(enemySize, enemySize, spawnFromLeft, spawnFromTop, 0, 10, 0, -10));
+
+                
             }
             else
             {
