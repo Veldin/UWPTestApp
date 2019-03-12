5526ecde4f40c76fd643afaa38041c6d8a938a7a
alwintje <alwinkroesen@gmail.com>
2019-03-12 11:47:19 +0100

    Better soundcontroller

--- EindopdrachtUWP/Classes/SoundController.cs
+++ EindopdrachtUWP/Classes/SoundController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Threading.Tasks;
+using Windows.Media.Audio;
+using Windows.Media.Render;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -9,97 +12,129 @@ namespace EindopdrachtUWP.Classes
 {
     class SoundController
     {
-        private Dictionary<string, Sound> sounds;
+        //private Dictionary<string, Sound> sounds;
+        private Dictionary<String, AudioFileInputNode> sounds;
 
-        MediaElement soundTrack = new MediaElement();
+        public AudioGraph graph;
+        private AudioFileInputNode backgroundMusicFileInput;
+        private AudioSubmixNode submixNode;
+        private AudioDeviceOutputNode deviceOutput;
         
         private bool mutedSFX = false;
         private bool mutedMusic = false;
 
+        private bool initialized = false;
+
+        private List<String> waitTillInitialized;
+
+
         public SoundController()
         {
-            sounds = new Dictionary<string, Sound>();
+            sounds = new Dictionary<string, AudioFileInputNode>();
+            waitTillInitialized = new List<string>();
             mutedSFX = false;
             mutedMusic = false;
-            soundTrack.Volume = 0.3;
-            soundTrack.IsLooping = true;
-            LoadSound("Soundtrack\\Soundtrack.wav", soundTrack);
+            this.initializeSoundSystem();
+        }
+
+        private async void initializeSoundSystem()
+        {
+            await CreateAudioGraph();
+
+            // If another file is already loaded into the FileInput node
+            if (backgroundMusicFileInput != null)
+            {
+                // Release the file and dispose the contents of the node
+                backgroundMusicFileInput.Dispose();
+            }
+
+            Windows.Storage.StorageFolder folder = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFolderAsync(@"Assets");
+            Windows.Storage.StorageFile file = await folder.GetFileAsync("Sounds\\" + "Soundtrack\\Soundtrack.wav");
+
+            CreateAudioFileInputNodeResult fileInputNodeResult = await graph.CreateFileInputNodeAsync(file);
+            if (fileInputNodeResult.Status != AudioFileNodeCreationStatus.Success)
+            {
+                // Cannot read file
+                Debug.WriteLine(String.Format("Cannot read input file because {0}", fileInputNodeResult.Status.ToString()));
+                return;
+            }
+                
+            backgroundMusicFileInput = fileInputNodeResult.FileInputNode;
+            backgroundMusicFileInput.OutgoingGain = 0.3;
+            backgroundMusicFileInput.AddOutgoingConnection(deviceOutput);
+
+
+            graph.Start();
+            initialized = true;
+
+            foreach(String filename in waitTillInitialized)
+            {
+                AddSound(filename);
+            }
+
         }
 
         public void AddSound(string sound, double volume = 0.8)
         {
             if (sound == null || sound == "") return;
             if (sounds.ContainsKey(sound)) return;
-
-            Sound s = new Sound()
+            if (!initialized)
             {
-                firstSound = new MediaElement(),
-                secondSound = new MediaElement(),
-                playFirst = true
-            };
-            s.firstSound.AutoPlay = false;
-            s.secondSound.AutoPlay = false;
-
-            s.firstSound.Volume = volume;
-            s.secondSound.Volume = volume;
-
-            sounds.Add(sound, s);
-            LoadSound(sound, s.firstSound);
-            LoadSound(sound, s.secondSound);
-            s.firstSound.MediaFailed += FailedLoadingMedia;
-            s.secondSound.MediaFailed += FailedLoadingMedia;
-        }
-
-        private void FailedLoadingMedia(object sender, ExceptionRoutedEventArgs e)
-        {
-            MediaElement me = (MediaElement)sender;
-            me.Source = null;
+                waitTillInitialized.Add(sound);
+            }
+            else
+            {
+                LoadSound(sound);
+            }
         }
 
         public void PlaySound(string sound)
         {
-            if (mutedSFX) return;
-            if (sounds.ContainsKey(sound))
+            if(sounds == null || sounds[sound] == null || mutedSFX)
             {
-                Sound toPlay = sounds[sound];
-                if (toPlay.playFirst)
-                {
-                    Play(toPlay.firstSound);
-                }
-                else
-                {
-                    Play(toPlay.secondSound);
-                }
-                toPlay.playFirst = !toPlay.playFirst;
+                return;
             }
+            
+            if(sounds[sound].OutgoingConnections.Count == 1)
+            {
+                sounds[sound].Reset();
+            }
+            else
+            {
+                sounds[sound].AddOutgoingConnection(submixNode);
+            }
+            
         }
-
-        // Must be in another method (because of async)
-        private async void LoadSound(string filename, MediaElement sound)
+        
+        private async void LoadSound(String filename)
         {
             Windows.Storage.StorageFolder folder = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFolderAsync(@"Assets");
+            Debug.WriteLine("test1");
             Windows.Storage.StorageFile file = await folder.GetFileAsync("Sounds\\" + filename);
-            var stream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
-            sound.SetSource(stream, file.ContentType);
-        }
 
-        private void Play(MediaElement sound)
-        {
-            try
-            {
-                Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
-                () =>
-                {
-                    sound.Position = new TimeSpan(0);
-                    sound.Play();
-                });
+            Debug.WriteLine("test2");
+            //Debug.WriteLine(graph == null);
+            if (graph == null) {
+                initializeSoundSystem();
             }
-            catch (Exception e)
+            CreateAudioFileInputNodeResult fileInputNodeResult = await graph.CreateFileInputNodeAsync(file);
+            Debug.WriteLine("test3");
+            if (fileInputNodeResult.Status != AudioFileNodeCreationStatus.Success)
             {
-                //e.Message
-                Debug.Write(e.Message);
+                // Cannot read file
+                Debug.WriteLine(String.Format("Cannot read input file because {0}", fileInputNodeResult.Status.ToString()));
+                return;
             }
+
+            Debug.WriteLine("test4");
+            sounds.Add(filename, fileInputNodeResult.FileInputNode);
+            Debug.WriteLine("test5");
+            sounds[filename].OutgoingGain = 1;
+            Debug.WriteLine("test6");
+
+            Debug.WriteLine(String.Format("Added sound {0}", filename));
         }
+        
 
         public void muteSFX()
         {
@@ -112,24 +147,55 @@ namespace EindopdrachtUWP.Classes
 
         public void muteMusic()
         {
-            Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
-            () =>
-                {
-                    mutedMusic = true;
-                    soundTrack.Pause();
-                }
-            );
+            if (backgroundMusicFileInput.OutgoingConnections.Count == 1)
+            {
+                backgroundMusicFileInput.RemoveOutgoingConnection(deviceOutput);
+            }
         }
 
         public void unMuteMusic()
         {
-            Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
-            () =>
-                {
-                    mutedMusic = false;
-                    soundTrack.Play();
-                }
-            );
+            if (backgroundMusicFileInput.OutgoingConnections.Count == 0)
+            {
+                backgroundMusicFileInput.AddOutgoingConnection(deviceOutput);
+                backgroundMusicFileInput.Reset();
+            }
         }
+
+
+        private async Task CreateAudioGraph()
+        {
+            // Create an AudioGraph with default settings
+            AudioGraphSettings settings = new AudioGraphSettings(AudioRenderCategory.Media);
+            CreateAudioGraphResult result = await AudioGraph.CreateAsync(settings);
+
+            if (result.Status != AudioGraphCreationStatus.Success)
+            {
+                // Cannot create graph
+                Debug.WriteLine(String.Format("AudioGraph Creation Error because {0}", result.Status.ToString()));
+                return;
+            }
+            
+            graph = result.Graph;
+
+            // Create a device output node
+            CreateAudioDeviceOutputNodeResult deviceOutputNodeResult = await graph.CreateDeviceOutputNodeAsync();
+
+            if (deviceOutputNodeResult.Status != AudioDeviceNodeCreationStatus.Success)
+            {
+                // Cannot create device output node
+                Debug.WriteLine(String.Format("Device Output unavailable because {0}", deviceOutputNodeResult.Status.ToString()));
+                return;
+            }
+
+            deviceOutput = deviceOutputNodeResult.DeviceOutputNode;
+
+            submixNode = graph.CreateSubmixNode();
+            submixNode.AddOutgoingConnection(deviceOutput);
+
+            Debug.WriteLine("Device Output Node successfully created");
+
+        }
+
     }
 }
